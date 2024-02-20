3d889e8f5c52c584834e834c77b99a80c6211431
unknown <elsie.dh@gmail.com>
2024-02-19 23:23:49 -0800

    added resume

--- ed-portfolio/src/pages/Resume.jsx
+++ ed-portfolio/src/pages/Resume.jsx
@@ -4,11 +4,29 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default function Resume() {
+  const onButtonClick = () => {
+    const pdfUrl = "ElsieResume.pdf";
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "ElsieResume.pdf"; // specify the filename
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
     return (
       <Container>
       <Row>
         <Col>
-        <h1>THIS IS THE RESUME PAGE</h1>
+        <center>
+                <h2>Interested in my background?</h2>
+                <h3>
+                    Click below to download a copy of my resume.
+                </h3>
+                <button onClick={onButtonClick}>
+                    Download PDF
+                </button>
+            </center>
    
       </Col>
       </Row>
